d1d8078c5c7d38ea26d5315e243d2844d5888f82
emzei <emzei.j@gmail.com>
2022-10-04 00:08:15 +0900

    Replace expo-app-loading -> SplashScreen module

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import AppLoading from "expo-app-loading";
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { Ionicons } from '@expo/vector-icons';
 import * as Font from 'expo-font';
 import { Asset } from 'expo-asset';
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
+  const [appIsReady, setAppIsReady] = useState(false); 
   const onFinish = () => setLoading(false);
   const preload = () => {
     const fontsToLoad = [Ionicons.font];
@@ -17,17 +18,44 @@ export default function App() {
     return Promise.all([...fontPromises, ...imagePromises]);
   };
 
-  if (loading) {
-    return (
-      <AppLoading
-        startAsync={preload}
-        onError={console.warn}
-        onFinish={onFinish}
-      />
-    );
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        // (e.g) await Font.loadAsync(Entypo.font);
+        await preload();
+        
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        // (e.g) await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Text>Hello</Text>
       <StatusBar style="auto" />
     </View>
